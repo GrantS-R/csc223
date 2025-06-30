5b18a469f4b4e32df77d3d2584bd57525c1fc826
GrantSR <gstaffordrenard@gmail.com>
2025-06-30 11:40:03 -0400

    removed test.java

--- Module3/DiscussionBoard/test.java
+++
@@ -1,7 +0,0 @@
-package Module3.DiscussionBoard;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Current working directory: " + System.getProperty("user.dir"));
-    }
-}
